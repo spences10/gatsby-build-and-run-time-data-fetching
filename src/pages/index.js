70e5c6e7c9f5af056b590096697b0a42370111f0
Scott Spence <scott.spence@karmarama.com>
2020-01-14 16:58:17 +0000

    digi

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import SEO from '../components/seo'
 export default () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi MMT London!</h1>
+    <h1>Hi MMT Digital!</h1>
     <p>Default Gatsby starter with data fetching examples.</p>
     <h3>
       <Link to="/build-time-data/">Build time Data Example</Link>
